72fb46bc76af2a27c29379847043b3ead2c06080
Zaid <63342254+azaidrahman@users.noreply.github.com>
2025-05-21 11:34:57 +0800

    render functions

---
+++ src/pages/lecture/day6-renderFunctions.vue
@@ -0,0 +1,9 @@
+<template>
+  <RenderComponent title="My Title">
+    This is the content
+  </RenderComponent>
+</template>
+
+<script setup>
+  import RenderComponent from '@/components/RenderComponent.vue';
+</script>
