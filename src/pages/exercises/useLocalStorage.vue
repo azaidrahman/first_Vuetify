447fb8851cfae5802173a93194408fbe425b9ac8
Zaid <63342254+azaidrahman@users.noreply.github.com>
2025-05-22 10:04:09 +0800

    composables-using local storage

---
+++ src/pages/exercises/useLocalStorage.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn
+    class="ma-4"
+    @click="
+      localValue = localValue === 'HELLO' ? 'BYE' : 'HELLO'
+    "
+  >
+    TOGGLE TEXT ({{ localValue }})
+  </v-btn>
+</template>
+
+<script setup>
+  import { useLocalStorage } from '@/composables/useLocalStorage';
+  const localValue = useLocalStorage('kee', 'HELLO');
+</script>
