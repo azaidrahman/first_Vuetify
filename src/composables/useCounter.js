5c8204f3fd36244fdd71d9471074737245be8ce0
Zaid <63342254+azaidrahman@users.noreply.github.com>
2025-05-22 10:03:14 +0800

    Composables

---
+++ src/composables/useCounter.js
@@ -0,0 +1,11 @@
+export function useCounter(initialValue = 0) {
+  const count = ref(initialValue);
+  const increment = () => {
+    count.value++;
+  };
+  const decrement = () => {
+    count.value--;
+  };
+
+  return { count, increment, decrement };
+}
---
+++ src/pages/lecture/composables.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-btn @click="decrement" class="ma-3">-</v-btn>
+    <span>{{ count }}</span>
+    <v-btn @click="increment" class="ma-3">+</v-btn>
+  </div>
+</template>
+
+<script setup>
+  import { useCounter } from '@/composables/useCounter';
+
+  const { count, increment, decrement } = useCounter(0);
+</script>
